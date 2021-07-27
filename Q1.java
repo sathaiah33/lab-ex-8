cd7e98ee07ef7692770dbe963b6cf6d328ce698b
sathaiah33 <88027170+sathaiah33@users.noreply.github.com>
2021-07-27 14:08:12 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labno8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author rakesh
+ */
+public class Q1{
+    public static void main(String args[])
+   {
+      String str, rev = "";
+      Scanner sc = new Scanner(System.in);
+ 
+      System.out.println("Enter a string:");
+      str = sc.nextLine();
+ 
+      int length = str.length();
+ 
+      for ( int i = length - 1; i >= 0; i-- )
+         rev = rev + str.charAt(i);
+ 
+      if (str.equals(rev))
+         System.out.println(str+" is a palindrome");
+      else
+         System.out.println(str+" is not a palindrome");
+ 
+   }
+    
+}
---
+++ Q2.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labno8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author rakesh
+ */
+public class Q2 {
+    public static void main(String[] args) {  
+          
+  Scanner sc = new Scanner(System.in);
+ 
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+        int len = str.length();
+        System.out.println("Enter no of equal parts:");
+        int n =sc.nextInt();  
+        int temp = 0, chars = len/n;  
+        
+        String[] subStr = new String [n];  
+         
+        if(len % n != 0) {  
+            System.out.println("Sorry this string cannot be divided into "+ n +" equal parts.");  
+        }  
+        else {  
+            for(int i = 0; i < len; i = i+chars) {  
+                //Dividing string in n equal part using substring()  
+                String part = str.substring(i, i+chars);  
+                subStr[temp] = part;  
+                temp++;  
+            }  
+    System.out.println(n + " equal parts of given string are ");  
+            for(int i = 0; i < subStr.length; i++) {  
+                System.out.println(subStr[i]);  
+                }  
+            }  
+        }  
+}  
+    
